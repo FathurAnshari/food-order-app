6b60f3608143ea397388e409759d500cadf69ac4
FathurAnshari <anshariF17@gmail.com>
2022-05-08 14:22:14 +0700

    cart-backdrop

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onTouch={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table of delicious meals" />
